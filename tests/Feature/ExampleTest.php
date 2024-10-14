bb900ff55a7948c5fd12e52bb4c5e775e8654e64
Rustam <rustamergashev.sp@gmail.com>
2024-10-14 20:48:46 +0500

    MINOR: Added some basic tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,10 +3,16 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      */
@@ -15,5 +21,12 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function testFetchingOneProduct(): void
+    {
+        $product = Product::factory()->create();
+
+        $this->assertAuthenticatedAs(User::find(1), 'auth')->get('/api/products/');
     }
 }
